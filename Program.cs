fe37920db4c5b3ccd5eb6ce8efc0d6b4478c5b5f
Tewodros <teddydesta2021@gmail.com>
2024-01-27 21:36:58 +0200

    updated code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Ajara!");
+
+string firstName= "Tewodros";
+string lastName = "Desta";
+
+//string interpolation in c#
+Console.WriteLine($"My first name is {firstName} and my father name is {lastName}");
+
+//string concatination
+ Console.WriteLine("my first name is" + firstName + "and " + " my father name is:" + lastName);
+
+//trim()
+string name= "     Tewodros     ";
+
+Console.WriteLine(name);
+Console.WriteLine(name.Trim());
--- obj/Debug/net8.0/helloWorld.AssemblyInfo.cs
+++ obj/Debug/net8.0/helloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("helloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5913f08067e1eacdb375f3a57422c3a8ba6ff07f")]
 [assembly: System.Reflection.AssemblyProductAttribute("helloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("helloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
