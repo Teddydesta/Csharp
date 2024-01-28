0390cc7c650626a827107741df78dc98de81842f
Tewodros <teddydesta2021@gmail.com>
2024-01-28 22:08:15 +0300

    code added

--- Program.cs
+++ Program.cs
@@ -1,18 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Ajara!");
+// Console.WriteLine("Hello, Ajara!");
 
 string firstName= "Tewodros";
 string lastName = "Desta";
-string name = "my first name is    " + firstName + "and     " + "    my father name is:" + lastName;
+string name = "my first name is    " + firstName + "   and     " + "  my father name is: " + lastName;
 //string interpolation in c#
-Console.WriteLine($"My first name is    {firstName}      and   my father name is {lastName}");
+// Console.WriteLine($"My first name is    {firstName}      and   my father name is {lastName}");
 
 //string concatination
  Console.WriteLine();
 
 //trim()
 // string name1= "     Tewodros     ";
-Console.WriteLine(name.Trim()); 
+// Console.WriteLine(name.Trim()); 
 
-//search string and replace
-Console.WriteLine(name.Replace("Tewodros", "Teddy"));
+//search string and replace string
+// Console.WriteLine(name.Replace("Tewodros", "Teddy"));
+
+//find for string in the code
+bool myName= name.Contains("Tewodros");
+Console.WriteLine(myName);
+
+//to upper
+Console.WriteLine(name.ToUpper());
+//length
+Console.WriteLine(name.Length);
--- obj/Debug/net8.0/helloWorld.AssemblyInfo.cs
+++ obj/Debug/net8.0/helloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("helloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe37920db4c5b3ccd5eb6ce8efc0d6b4478c5b5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+170c16827db8e011e0daef668a05a07d9696cc6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("helloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("helloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
