710ab4b455ba6ccd000cd65198d3b815bc8c9207
Tewodros <teddydesta2021@gmail.com>
2024-01-29 22:35:18 +0300

    code added

--- Program.cs
+++ Program.cs
@@ -1,22 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, Ajara!");
+﻿void Fundamentals(){
+Console.WriteLine("Hello, Ajara!");
 
 string firstName= "Tewodros";
 string lastName = "Desta";
 string name = "my first name is    " + firstName + "   and     " + "  my father name is: " + lastName;
 
 //string interpolation in c#
-// Console.WriteLine($"My first name is    {firstName}      and   my father name is {lastName}");
+Console.WriteLine($"My first name is    {firstName}      and   my father name is {lastName}");
 
 //string concatination
  Console.WriteLine();
 
 //trim()
-// string name1= "     Tewodros     ";
-// Console.WriteLine(name.Trim()); 
+string name1= "     Tewodros     ";
+Console.WriteLine(name.Trim()); 
 
 //search string and replace string
-// Console.WriteLine(name.Replace("Tewodros", "Teddy"));
+Console.WriteLine(name.Replace("Tewodros", "Teddy"));
 
 
 //find for string in the code
@@ -27,11 +27,14 @@ Console.WriteLine(myName);
 Console.WriteLine(name.ToUpper());
 //length
 Console.WriteLine(name.Length);
+}
 
 //
 // Numbers in C#
 
+// workingWithNumber();
 
+void workingWithNumber(){
 int a= 2100000000; 
 int b= 2100000000;
 int c =  a + b;
@@ -48,4 +51,19 @@ int g= 2100000000;
 int h= 2100000000;
 long i =  checked(g + h);
 Console.WriteLine(i);
+
+}
+// OrderPrecedence();
+void OrderPrecedence(){
+    int a = 10;
+    int b = 12;
+    int c =  2;
+    int d =  a + b * c;
+    Console.WriteLine(d);
+
+    d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+    Console.WriteLine(d);
+
+    
+}
 
